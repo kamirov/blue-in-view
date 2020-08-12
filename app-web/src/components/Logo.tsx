5fc45c025ece230543dde2039c4032b6ac9545ad
kamirov <andrei.khramtsov@gmail.com>
2020-08-12 07:53:01 -0400

    [FEAT] Use local logo

--- app-web/src/components/Logo.tsx
+++ app-web/src/components/Logo.tsx
@@ -1,10 +1,7 @@
 import * as React from 'react'
 import styled from '@emotion/styled'
 
-import { Assets } from "../library/assets.module";
-
-const logoVertical = Assets.getMediaUrl('logo-vertical.png')
-const logoHorizontal = Assets.getMediaUrl('logo-horizontal.png')
+import logo from "../assets/images/logo.png"
 
 const StyledImg = styled.img`
     width: 100%
@@ -15,9 +12,7 @@ type Props = {
     vertical?: boolean
 }
 
-const Logo: React.FC<Props> = ({className, vertical}) => {
-
-    const logo = vertical ? logoVertical : logoHorizontal
+const Logo: React.FC<Props> = ({className}) => {
 
     return <StyledImg className={className} src={logo} alt="Logo" />
 }
