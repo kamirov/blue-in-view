58f322faff2fe330427b0e7d6db18582bb554997
kamirov <andrei.khramtsov@gmail.com>
2020-08-12 07:53:24 -0400

    [REFAC] Simplify props

--- app-web/src/components/VideoPlayer.tsx
+++ app-web/src/components/VideoPlayer.tsx
@@ -1,36 +1,21 @@
 import * as React from 'react'
-import styled from '@emotion/styled'
-import { VideoFormatMap } from "../domain/videos/video.module";
 
 type Props = {
     className?: string
-    videos: VideoFormatMap
-    screenshot: string
-    isMuted?: boolean
+    url: string
+    title: string
 }
 
-// @ts-ignore TODO: Implement screenshots
-const VideoPlayer: React.FC<Props> = ({className, videos, screenshot, isMuted}) => {
+const VideoPlayer: React.FC<Props> = ({className, url, title}) => {
 
-    const videoTypes = Object.keys(videos)
-
-    // We need a new key, otherwise React won't re-render the video when the source changes
-    // @ts-ignore TODO: Fix why we TS complains when we use the format-map video type as the key
-    const key = videos[videoTypes[0]]
-
-    return <Container className={className}>
-        <StyledVideo muted={isMuted} controlsList="nodownload" autoPlay controls key={key} poster={screenshot}>
-            {videos.mp4 && <source src={videos.mp4} type="video/mp4" />}
-        </StyledVideo>
-    </Container>
-
+    return <iframe
+        className={className}
+        src={url}
+        title={title}
+        allow="accelerometer; encrypted-media; gyroscope; picture-in-picture"
+        frameBorder="0"
+        allowFullScreen
+    />
 }
-
-const Container = styled.div`
-`
-
-const StyledVideo = styled.video`
-    width: 100%;
-`
 
 export default VideoPlayer
